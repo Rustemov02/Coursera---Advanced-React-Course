c68e83e33371a9fa20995b513b0c080295429450
Bunyamin <bunyamin2002@mail.ru>
2025-09-18 07:35:57 +0400

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const data = [
+  {
+    id: 1,
+    title: "Laptop",
+    description: "Lightweight laptop with 16GB RAM",
+    price: 1200,
+  },
+  {
+    id: 2,
+    title: "Headphones",
+    description: "Noise-cancelling over-ear headphones",
+    price: 200,
+  },
+  {
+    id: 3,
+    title: "Smartphone",
+    description: "5G smartphone with 128GB storage",
+    price: 800,
+  },
+];
+
+const topDesserts = data.map((dessert) => {
+  return {
+    content: `${dessert.title} - ${dessert.description}`,
+    price: dessert.price,
+  };
+});
+
+const listItems = data.map((desert) => {
+  const itemText = `${desert.title} - ${desert.price}`;
+
+  return <li>{itemText}</li>;
+});
+
+export default function App() {
+  console.log(topDesserts);
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  );
+}
