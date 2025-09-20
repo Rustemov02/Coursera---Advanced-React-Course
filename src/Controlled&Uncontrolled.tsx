19fefdda3c8611a109686d1c14e111de7c48855d
Bunyamin <bunyamin2002@mail.ru>
2025-09-20 08:36:16 +0400

    created Register Form

--- src/Controlled&Uncontrolled.tsx
+++ src/Controlled&Uncontrolled.tsx
@@ -1,19 +1,49 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 const ContAndUnCont = () => {
-  const inputRef = useRef(null);
-  const handleSubmit = (e: any) => {
+  const [score, setScore] = useState("10");
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const inputValue = inputRef?.current?.value;
+    if (Number(score) < 5 && comment.length <= 10) {
+      alert("Please provide a comment explaining why the experience was poor.");
+      return;
+    }
 
-    console.log(inputValue);
+    console.log("Form Submitted !");
+    setScore("10");
+    setComment("");
   };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input ref={inputRef} type="text"/>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset className="border w-1/2 flex flex-col items-start gap-4">
+          <h2>Feedback Form</h2>
+          <div className="flex flex-col items-start">
+            <label htmlFor="name">Score : {score}</label>
+            <input
+              id="score"
+              type="range"
+              min="0"
+              max="10"
+              name="score"
+              value={score}
+              onChange={(e) => setScore(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <label>Comment :</label>
+            <textarea
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              className="outline-1 rounded-xl"
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+    </>
   );
 };
 
